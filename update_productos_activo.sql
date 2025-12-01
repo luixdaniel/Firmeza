00c1697d94e165bf96adabad5d41b3ff2d431cb5
luis daniel <ceraluis4@gmail.com>
2025-12-01 16:13:56 -0500

    feat: finish definitive

--- update_productos_activo.sql
+++
@@ -1,6 +0,0 @@
--- Actualizar todos los productos existentes para que estén activos
-UPDATE "Productos" SET "Activo" = true WHERE "Activo" = false;
-
--- Verificar el resultado
-SELECT "Id", "Nombre", "Activo", "Stock" FROM "Productos";
-
